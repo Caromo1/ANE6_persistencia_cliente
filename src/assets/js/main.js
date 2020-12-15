8226ae0d52e5a421107c32bbcdf4d2e693b121f6
caromo <caromo@floridauniversitaria.es>
2020-12-15 12:14:41 +0100

    Añadiendo libreria moment.js

---
+++ src/assets/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded"), ()=> {
+        const datosform = document.getElementById("formulario");
+}
+
+function guardadatos(){
+        const usu=document.getElementById("usuario").value;
+        const contr=document.getElementById("contra").value;
+        localStorage.setItem("usuario", usu);
+        localStorage.setItem("contraseña", contr);
+        const data = moment().format('YYYY-MM-DDTHH:mm');
+        let usulocal = localStorage.getItem("usuario");
+        let contrlocal = localStorage.getItem("contraseña");
+        console.log(data,usulocal,contrlocal);
+        var x = document.getElementById("formulario");
+        if(usulocal=="" || contrlocal==""){      
+                alert("rellena")
+        }else{
+               x.style.display = "none";
+               function formObj(contrlocal,usulocal) {
+                this.contrlocal=contrlocal;
+                this.usulocal=document.usulocal;
+                var formularidatos=new formObj(contrlocal,usulocal);
+                document.getElementById("musu").textContent=formularidatos.usulocal;
+                document.getElementById("mcontra").textContent=formularidatos.contrlocal;
+              }
+        }
+}
